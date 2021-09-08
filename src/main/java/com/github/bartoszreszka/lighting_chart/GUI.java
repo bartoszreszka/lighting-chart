c7febef3d998030fd05e4c653dc7ce7f2e4feb31
Bartosz Reszka <br.reszka@gmail.com>
2021-09-08 22:55:20 +0200

    Parsing coordinates.

--- src/main/java/com/github/bartoszreszka/lighting_chart/GUI.java
+++ src/main/java/com/github/bartoszreszka/lighting_chart/GUI.java
@@ -9,9 +9,12 @@ public class GUI extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JSpinner spinner1;
-    private JComboBox comboBox1;
-    private JTextField textField1;
-    private JTextField textField2;
+    private JComboBox predefinedHarboursComboBox;
+    private JTextField latDegField;
+    private JTextField longDegField;
+    private JTextField latMinutesField;
+    private JTextField longMinutesField;
+    private JTextField pointNameField;
 
     public GUI() {
         setContentPane(contentPane);
@@ -24,13 +27,43 @@ public class GUI extends JDialog {
             }
         });
 
-        comboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(
+        predefinedHarboursComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(
                 Arrays.stream(Locations.values()).map(Locations::locName).toArray(String[]::new)
         ));
     }
 
     private void onOK() {
-        // add your code here
+        Computations.location = parseCoords();
         dispose();
+    }
+
+    private Location parseCoords() {
+        Location location;
+        double  lat,
+                lng;
+        int     latDeg,
+                lngDeg;
+        double  latMinutes,
+                lngMinutes;
+
+        // Parsing degrees and minutes given separately:
+        if (!latMinutesField.getText().isEmpty() || !longMinutesField.getText().isEmpty()) {
+            latDeg = Integer.parseInt(latDegField.getText());
+            latMinutes = Double.parseDouble(latMinutesField.getText());
+            lngDeg = Integer.parseInt(longDegField.getText());
+            lngMinutes = Double.parseDouble(longMinutesField.getText());
+            lat = latDeg + (latMinutes / 60);
+            lng = lngDeg + (lngMinutes / 60);
+        // Parsing degrees given in decimal format:
+        } else {
+            lat = Double.parseDouble(latDegField.getText());
+            lng = Double.parseDouble(longMinutesField.getText());
+        }
+        if (!pointNameField.getText().isEmpty()) {
+            location = new LocationCoordinates(lat, lng, pointNameField.getText());
+        } else {
+            location = new LocationCoordinates(lat, lng);
+        }
+        return location;
     }
 }
