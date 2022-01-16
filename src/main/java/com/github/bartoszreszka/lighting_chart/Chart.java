e9a9e6f17346200f4d4f4b382e2a7969d159f90a
Bartosz Reszka <br.reszka@gmail.com>
2022-01-16 15:07:26 +0100

    WindowListener added to skip back to main window.

--- src/main/java/com/github/bartoszreszka/lighting_chart/Chart.java
+++ src/main/java/com/github/bartoszreszka/lighting_chart/Chart.java
@@ -2,9 +2,7 @@ package com.github.bartoszreszka.lighting_chart;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class Chart extends JDialog {
     // <-- Change chart appearance here:
@@ -36,7 +34,7 @@ public class Chart extends JDialog {
     public Chart() {
         setTitle("Grafik Oświetlenia.");
         setContentPane(contentPane);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 //        setSize(600, 400);
         setResizable(true);
         setLocationRelativeTo(null);
@@ -59,11 +57,18 @@ public class Chart extends JDialog {
         });
 
         // Call onOK() when ESC button pressed
-        contentPane.registerKeyboardAction(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+        contentPane.registerKeyboardAction(
+                e -> onOK(),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
                 onOK();
             }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        });
+
     }
 
     private void onPrint() {
