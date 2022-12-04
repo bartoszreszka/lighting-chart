773c8ed06af09f57d1000f8707625def5721051f
Bartosz Reszka <br.reszka@gmail.com>
2022-12-04 14:45:23 +0000

    Icon added.

--- src/main/java/com/github/bartoszreszka/lighting_chart/view/About.java
+++ src/main/java/com/github/bartoszreszka/lighting_chart/view/About.java
@@ -15,6 +15,9 @@ public class About extends JDialog {
         setContentPane(contentPane);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
+        ImageIcon icon = new ImageIcon("eclipse.png");
+        setIconImage(icon.getImage());
+
         aboutLabel.setText("Program służy do obliczania i kreślenia \"grafiku oświetlenia\" dla wybranej " +
                 "lub wprowadzonej lokalizacji.");
         authorLabel.setText("Bartosz Reszka, 2022");
