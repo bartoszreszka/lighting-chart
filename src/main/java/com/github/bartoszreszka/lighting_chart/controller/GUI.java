dc2c2dad9b6f4626ec5793a3827086df983328fa
-br <47790254+bartoszreszka@users.noreply.github.com>
2023-01-12 23:16:56 +0000

    Coords validation

--- src/main/java/com/github/bartoszreszka/lighting_chart/controller/GUI.java
+++ src/main/java/com/github/bartoszreszka/lighting_chart/controller/GUI.java
@@ -2,8 +2,8 @@ package com.github.bartoszreszka.lighting_chart.controller;
 
 import com.github.bartoszreszka.lighting_chart.Main;
 import com.github.bartoszreszka.lighting_chart.model.*;
-import com.github.bartoszreszka.lighting_chart.view.About;
 import com.github.bartoszreszka.lighting_chart.view.Chart;
+import com.github.bartoszreszka.lighting_chart.view.About;
 import org.shredzone.commons.suncalc.MoonPhase;
 
 import javax.swing.*;
@@ -25,6 +25,7 @@ public class GUI extends JFrame {
     private JTextField longMinutesField;
     private JTextField pointNameField;
     private JButton buttonOK;
+    private Location location;
 
     public GUI() {
         setTitle(Main.getProgTitle() + " " + Main.getVersion());
@@ -40,9 +41,9 @@ public class GUI extends JFrame {
         predefinedHarboursComboBox.setModel(
                 new javax.swing.DefaultComboBoxModel<>(
                         Arrays.stream(Locations.values())
-                        .map(Locations::locName)
-                        .toArray(String[]::new)
-        ));
+                                .map(Locations::locName)
+                                .toArray(String[]::new)
+                ));
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -92,42 +93,35 @@ public class GUI extends JFrame {
     }
 
     private Location parseCoordsFromTextFields() {
-        Location location;
+        location = getLocationFromSpinner();
         double  lat,
                 lng;
-        int     latDeg,
-                lngDeg;
-        double  latMinutes,
-                lngMinutes;
 
         if (coordsAreProvided()) {
-            if (!latMinutesField.getText().isEmpty() || !longMinutesField.getText().isEmpty()) {
-                // Parse degrees and minutes given separately
-                latDeg = Integer.parseInt(latDegField.getText());
-                latMinutes = Double.parseDouble(latMinutesField.getText());
-                lngDeg = Integer.parseInt(longDegField.getText());
-                lngMinutes = Double.parseDouble(longMinutesField.getText());
-                lat = latDeg + (latMinutes / 60);
-                lng = lngDeg + (lngMinutes / 60);
-            } else {
-                // Parse degrees given in decimal format
+            try {
                 lat = Double.parseDouble(latDegField.getText().replaceAll("[,]", "."));
                 lng = Double.parseDouble(longDegField.getText().replaceAll("[,]", "."));
+                if (!(lat >= -90 && lat <= 90) || !(lng >=-180 && lng <= 180)) {
+                    return location;
+                }
+                if (!pointNameField.getText().isEmpty()) {
+                    location = new LocationCoordinates(lat, lng, pointNameField.getText());
+                } else {
+                    location = new LocationCoordinates(lat, lng);
+                }
+            } catch (NumberFormatException nfe) {
+                nfe.printStackTrace();
             }
-            if (!pointNameField.getText().isEmpty()) {
-                location = new LocationCoordinates(lat, lng, pointNameField.getText());
-            } else {
-                location = new LocationCoordinates(lat, lng);
-            }
-        } else {
-            // Get harbour selected from comboBox
-            location = Arrays.stream(Locations.values())
-                    .filter(locations -> locations.locName()
-                            .equals(predefinedHarboursComboBox.getSelectedItem().toString()))
-                    .collect(Collectors.toList())
-                    .get(0);
         }
         return location;
+    }
+
+    private Location getLocationFromSpinner() {
+        return location = Arrays.stream(Locations.values())
+                .filter(locations -> locations.locName()
+                        .equals(predefinedHarboursComboBox.getSelectedItem().toString()))
+                .collect(Collectors.toList())
+                .get(0);
     }
 
     private boolean coordsAreProvided() {
@@ -149,14 +143,14 @@ public class GUI extends JFrame {
 
     private void createUIComponents() {
         spinner1 = new JSpinner(new SpinnerNumberModel(
-                        Calendar.getInstance().get(Calendar.MONTH) + 1,
-                        1,
-                        12,
-                        1));
+                Calendar.getInstance().get(Calendar.MONTH) + 1,
+                1,
+                12,
+                1));
         spinner2 = new JSpinner(new SpinnerNumberModel(
-                        Calendar.getInstance().get(Calendar.YEAR),
-                        1900,
-                        2200,
-                        1));
+                Calendar.getInstance().get(Calendar.YEAR),
+                1900,
+                2200,
+                1));
     }
 }
